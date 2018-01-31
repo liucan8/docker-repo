9a9faf54edf68ff36cd03b1d0ae35fbf84e478ab
liucan8 <1074414731@qq.com>
2018-01-31 13:34:43 +0800

    adds

--- liucan-wechat/src/main/java/com/lc/service/WechatService.java
+++ liucan-wechat/src/main/java/com/lc/service/WechatService.java
@@ -19,25 +19,28 @@ public class WechatService {
     public String checkWechat(HttpServletRequest request){
         String token = "12";
 
-        String signature = request.getParameter("signature");
-        String timestamp = request.getParameter("timestamp");
-        String nonce = request.getParameter("nonce");
-        String echostr = request.getParameter("echostr");
+        try {
+            String signature = request.getParameter("signature");
+            String timestamp = request.getParameter("timestamp");
+            String nonce = request.getParameter("nonce");
+            String echostr = request.getParameter("echostr");
 
-        String params[] = {token,nonce,timestamp};
-        Arrays.sort(params);
+            String params[] = {token,nonce,timestamp};
+            Arrays.sort(params);
 
-        String encodeParam = "";
-        for(int i=0;i<3;i++) {
-            encodeParam += params[i];
+            String encodeParam = "";
+            for(int i=0;i<3;i++) {
+                encodeParam += params[i];
+            }
+            encodeParam = DigestUtils.md5DigestAsHex(encodeParam.getBytes());
+
+            if(encodeParam.equals(signature)) {
+                return echostr;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        encodeParam = DigestUtils.md5DigestAsHex(encodeParam.getBytes());
-
-        if(encodeParam.equals(signature)) {
-            return echostr;
-        } else {
-            return "1";
-        }
+        return "1";
     }
 
     public String getAccessToken(){
